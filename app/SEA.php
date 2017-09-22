fddbf252aab096922a4593dc23c143c86340052b
Arvind Narayanan <arvindonarayanan@gmail.com>
2017-09-23 00:47:45 +0530

    Add: all models

---
+++ app/SEA.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SEA extends Model
+{
+	protected $table = 'sea';
+    protected $primarykey = 'sea_id';
+    public $timestamps = false;
+}
